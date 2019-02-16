70a4236afb21ef9ef628afc4a65fe07e273cacce
Jonny <jonathon_lokken@yahoo.com>
2019-02-16 12:11:34 -0600

    Update PlayerManager.py

--- lavalink/PlayerManager.py
+++ lavalink/PlayerManager.py
@@ -126,9 +126,11 @@ class DefaultPlayer(BasePlayer):
                 track = track.replace('spotify', 'ytsearch')
                 track = await self._lavalink.get_tracks(track)
                 track = track['tracks'][0]
-            self.current = track
-            await self._lavalink.ws.send(op='play', guildId=self.guild_id, track=track.track)
-            await self._lavalink.dispatch_event(TrackStartEvent(self, track))
+                print(track)
+            else:
+                self.current = track
+                await self._lavalink.ws.send(op='play', guildId=self.guild_id, track=track.track)
+                await self._lavalink.dispatch_event(TrackStartEvent(self, track))
 
     async def play_now(self, requester: int, track: dict):
         """ Add track and play it. """
