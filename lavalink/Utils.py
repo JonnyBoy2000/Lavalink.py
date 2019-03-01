00174639667b2bfcaf6db7cee999e3710dad074b
Jonny <jonathon_lokken@yahoo.com>
2019-02-28 23:02:56 -0600

    Time changes.

--- lavalink/Utils.py
+++ lavalink/Utils.py
@@ -2,5 +2,9 @@ def format_time(time):
     """ Formats the given time into HH:MM:SS. """
     hours, remainder = divmod(time / 1000, 3600)
     minutes, seconds = divmod(remainder, 60)
-
-    return '%02d:%02d:%02d' % (hours, minutes, seconds)
+    if hours:
+        return '%02dh %02dm %02ds' % (hours, minutes, seconds)
+    if minutes:
+        return '%02dm %02ds' % (minutes, seconds)
+    if seconds:
+        return '%02ds' % (seconds)
